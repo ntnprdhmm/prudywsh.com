7677474548c1a62da8153738f982426bf094011d
prudywsh <antoineprudhomme5@gmail.com>
2018-07-21 22:12:42 +0200

    first commit: express + env + eslint

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
