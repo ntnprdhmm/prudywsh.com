0f435e93d574115877219dbc21c045079cc4dc8c
antoine prudhomme <antoine.prudhomme@spendesk.com>
2018-07-22 09:57:19 +0200

    vuejs added

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../..', 'client/dist')));
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.sendFile(path.resolve(__dirname, '../..', 'client/dist/index.html'));
 });
 
 app.listen(process.env.PORT, () => {
