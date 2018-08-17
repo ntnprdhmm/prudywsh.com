fab04250df3284ad88319f333deec4ea236a04a2
antoine prudhomme <antoineprudhomme5@gmail.com>
2018-08-17 20:29:23 +0000

    add https

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../..', 'client/dist')));
+app.use(express.static(path.resolve(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../..', 'client/dist/index.html'));
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}`);
-});
+app.get('/health-check', (req, res) => res.sendStatus(200));
+
+app.listen(process.env.PORT);
+
+if (process.env.NODE_ENV === 'production') {
+  const options = {
+    cert: fs.readFileSync(path.resolve(__dirname, 'sslcert/fullchain.pem')),
+    key: fs.readFileSync(path.resolve(__dirname, 'sslcert/privkey.pem')),
+  };
+  https.createServer(options, app).listen(443);
+}
+
